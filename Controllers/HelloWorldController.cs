758079e7d808e1f95ec1556fc7105576db92b8b8
Noel Lopes <noel@ipg.pt>
2021-10-13 09:38:15 +0100

    Added HelloWorld controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 7 && hour < 12)
+            {
+                ViewBag.Message = "Good Morning";
+            }
+            else if (hour >= 12 && hour < 20)
+            {
+                ViewBag.Message = "Good Afternoon";
+            } else
+            {
+                ViewBag.Message = "Good evening";
+            }
+            
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome
+        public string Welcome()
+        {
+            return "This is my Welcome action method ...";
+        }
+    }
+}
