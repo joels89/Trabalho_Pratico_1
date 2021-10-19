aea201023d8a0e4249d67ad6bc1895e9627eefd2
Noel Lopes <noel@ipg.pt>
2021-10-19 11:03:18 +0100

    Validate the guest response

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -4,19 +4,20 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PartyInvites.Models
-{
-    public class GuestResponse
-    {
-        [Required(ErrorMessage ="Please enter your name")]
-        public string Name { get; set; }
+namespace PartyInvites.Models {
+	public class GuestResponse {
+		[Required(ErrorMessage = "Please enter your name")]
+		[StringLength(100, MinimumLength = 3, ErrorMessage ="Name should have at least 3 chars and no more than 100 chars")]
+		public string Name { get; set; }
 
-        [Required(ErrorMessage ="Please enter your phone number")]
-        public string Phone { get; set; }
+		[Required(ErrorMessage = "Please enter your phone number")]
+		[Phone(ErrorMessage = "Invalid phone number")]
+		public string Phone { get; set; }
 
-        [Required(ErrorMessage ="Please enter your email address")]
-        public string Email { get; set; }
+		[Required(ErrorMessage = "Please enter your email address")]
+		[EmailAddress(ErrorMessage = "Invalid email adress")]
+		public string Email { get; set; }
 
-        public bool? WillAttend { get; set; }
-    }
+		public bool? WillAttend { get; set; }
+	}
 }
