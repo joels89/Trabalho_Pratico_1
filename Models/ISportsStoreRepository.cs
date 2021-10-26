d7f0c9c47779c61599cbb84778aca2d445880eb3
Noel Lopes <noel@ipg.pt>
2021-10-26 10:48:14 +0100

    Created interface for the sports store repository

---
+++ Models/ISportsStoreRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models {
+	interface ISportsStoreRepository {
+		public IEnumerable<Product> Products { get; }
+	}
+}
