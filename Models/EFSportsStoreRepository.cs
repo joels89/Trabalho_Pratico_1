d9ff5e948931ff44f9ea80bff5a53c341027787c
Noel Lopes <noel@ipg.pt>
2021-10-29 09:17:14 +0100

    Created an entity framework core repository

---
+++ Models/EFSportsStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using SportsStore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models {
+	public class EFSportsStoreRepository : ISportsStoreRepository {
+		private SportsStoreDbContext context;
+
+		public EFSportsStoreRepository(SportsStoreDbContext context) {
+			this.context = context;
+		}
+
+		public IEnumerable<Product> Products => context.Products;
+	}
+}
