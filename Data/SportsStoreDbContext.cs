ae06f78f4848005aaf4a6b37e0ce3123fa0900ef
Noel Lopes <noel@ipg.pt>
2021-10-29 09:11:48 +0100

    Created SportsStoreDbContext

---
+++ Data/SportsStoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Data {
+	public class SportsStoreDbContext : DbContext {
+		public SportsStoreDbContext(DbContextOptions<SportsStoreDbContext> options) : base(options) {
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
