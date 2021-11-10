8489a97a3a42a903222d4f3e4de7f223aac0c981
Noel Lopes <noel@ipg.pt>
2021-11-10 11:00:02 +0000

    Changed create view

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Books.Models {
 
 		public string Description { get; set; }
 
+		[DisplayName("Author")]
 		public int AuthorId { get; set; }
 		public Author Author { get; set; }
 	}
