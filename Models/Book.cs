b1ae5d7271c940cd38ee17f379f1d3fced783c98
Noel Lopes <noel@ipg.pt>
2021-11-10 08:56:06 +0000

    Created class to represent books

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models {
+	public class Book {
+		public int BookId { get; set; }
+
+		[Required]
+		[StringLength(512)]
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+	}
+}
