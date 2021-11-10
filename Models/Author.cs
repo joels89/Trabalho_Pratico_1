14f655b2f6fb68ddc4def7caeffbe27f1bdbd074
Noel Lopes <noel@ipg.pt>
2021-11-10 08:51:51 +0000

    Crerated class to represent authors

---
+++ Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models {
+	public class Author {
+		public int AuthorId { get; set; }
+
+		[Required]
+		[StringLength(256)]
+		public string Name { get; set; }
+	}
+}
