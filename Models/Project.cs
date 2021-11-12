15f0c6845b634ab9185e8032f7f95f949b0ac29b
Joel SOUSA <joel.sousa@altran.com>
2021-11-12 08:53:31 +0000

    add class project

---
+++ Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoPratico_1.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        public string Client { get; set; }
+
+        public ICollection<SoftwareRequirement> Srq { get; set; }
+    }
+}
