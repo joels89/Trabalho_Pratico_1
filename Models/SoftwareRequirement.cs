a9ff3868504d2a09a7f930bdb900f79d7b05be30
Joel SOUSA <joel.sousa@altran.com>
2021-11-12 08:54:07 +0000

    add class Software Requirements

---
+++ Models/SoftwareRequirement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoPratico_1.Models
+{
+    public class SoftwareRequirement
+    {
+        public int SoftwareReqId { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string SoftwareReqTitle { get; set; }
+
+        public string SoftwareReqDescription { get; set; }
+
+        public Project Project { get; set; }
+
+    }
+}
