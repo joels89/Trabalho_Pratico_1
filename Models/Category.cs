b249382dcdf4e24eb006fd417d24bbb08592f311
Noel Lopes <noel@ipg.pt>
2021-11-17 10:26:16 +0000

    Define category model

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models {
+	public class Category {
+		public int CategoryId { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Name { get; set; }
+	}
+}
