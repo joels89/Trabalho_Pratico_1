5a58a78e0f69e0c214b4f6c966b21bf6f03cd742
Noel Lopes <noel@ipg.pt>
2021-11-17 10:51:31 +0000

    Add relationships to BookCategory (using fluent API)

--- Data/BooksContext.cs
+++ Data/BooksContext.cs
@@ -17,9 +17,19 @@ namespace Books.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<BookCategory>()
                 .HasKey(bc => new { bc.BookId, bc.CategoryId });
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(bc => bc.Book)
+                .WithMany(b => b.BookCategories)
+                .HasForeignKey(bc => bc.BookId);
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(bc => bc.Category)
+                .WithMany(c => c.CategoryBooks)
+                .HasForeignKey(bc => bc.CategoryId);
         }
 
-		public DbSet<Books.Models.Book> Book { get; set; }
+        public DbSet<Books.Models.Book> Book { get; set; }
 
         public DbSet<Books.Models.Author> Author { get; set; }
     }
