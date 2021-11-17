0f5eedb943c7b625c80e0689b7ed3a0899cb5222
Noel Lopes <noel@ipg.pt>
2021-11-17 09:05:36 +0000

    Define paging info class

---
+++ ViewModels/PagingInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.ViewModels {
+	public class PagingInfo {
+		public int TotalItems { get; set; }
+		public int PageSize { get; set; } = 10;
+		public int CurrentPage { get; set; }
+		public int TotalPages => 
+			(int)Math.Ceiling((double)TotalItems / PageSize);
+	}
+}
