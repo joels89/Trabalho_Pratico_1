e7a78b5053388e29a84548737878c523f461bc22
Noel Lopes <noel@ipg.pt>
2021-11-17 09:09:16 +0000

    Created view model to show the book list (book/index)

---
+++ ViewModels/BookListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Books.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.ViewModels {
+	public class BookListViewModel {
+		public IEnumerable<Book> Books { get; set; }
+		public PagingInfo PagingInfo { get; set; }
+	}
+}
