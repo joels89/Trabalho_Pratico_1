30b8f1ffa0f41d9291f9bdeccde90491138536a9
Noel Lopes <noel@ipg.pt>
2021-11-17 10:50:47 +0000

    Add composed primary key for BookCategory

--- Data/BooksContext.cs
+++ Data/BooksContext.cs
@@ -14,7 +14,12 @@ namespace Books.Data
         {
         }
 
-        public DbSet<Books.Models.Book> Book { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.CategoryId });
+        }
+
+		public DbSet<Books.Models.Book> Book { get; set; }
 
         public DbSet<Books.Models.Author> Author { get; set; }
     }
